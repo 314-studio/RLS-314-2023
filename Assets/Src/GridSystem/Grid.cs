a4829b0d90ebb699e44bd92435e212ba829379b2
Sunbvert <sunchuanop@outlook.com>
2023-02-10 00:20:43 +0100

    添加了Generic Grid

---
+++ Assets/Src/GridSystem/Grid.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Src.GridSystem
+{
+    internal class Grid<TGridObject>
+    {
+        private int _width;
+        private int _depth;
+        private float _cellSize;
+        private float _halfCellSize;
+        private Vector3 _leftBottom;
+
+        private TGridObject[,] _gridArray;
+        
+
+        public Grid(int width, int depth, float cellSize, Vector3 originPosition)
+        {
+            Vector3 leftBottom = new Vector3(originPosition.x - width * cellSize / 2, 0,
+                originPosition.z - depth * cellSize / 2);
+            _width = width;
+            _depth = depth;
+            _cellSize = cellSize;
+            _halfCellSize = _cellSize / 2;
+            _leftBottom = leftBottom;
+
+            _gridArray = new TGridObject[width, depth];
+        }
+
+        public void GetSize(out int width, out int depth)
+        {
+            width = _width;
+            depth = _depth;
+        }
+
+        public float GetCellSize()
+        {
+            return _cellSize;
+        }
+
+        public void SetValue(int x, int z, TGridObject value)
+        {
+            if (x >= 0 && z >= 0 && x < _width && z < _depth)
+            {
+                _gridArray[x, z] = value;
+            }
+        }
+
+        public void SetValue(Vector3 worldPosition, TGridObject value)
+        {
+            GetGridPosition(worldPosition, out var x, out var z);
+            SetValue(x, z, value);
+        }
+
+        public TGridObject GetValue(int x, int z)
+        {
+            if (x >= 0 && z >= 0 && x < _width && z < _depth)
+            {
+                return _gridArray[x, z];
+            }
+            else
+            {
+                return default(TGridObject);
+            }
+        }
+
+        public TGridObject GetValue(Vector3 worldPosition)
+        {
+            GetGridPosition(worldPosition, out var x, out var z);
+            return GetValue(x, z);
+        }
+        
+        public Vector3 GetWorldPosition(int x, int z)
+        {
+            return new Vector3(x, 0, z) * _cellSize + _leftBottom;
+        }
+
+        public void GetGridPosition(Vector3 worldPosition, out int x, out int z)
+        {
+            x = Mathf.FloorToInt((worldPosition - _leftBottom).x / _cellSize);
+            z = Mathf.FloorToInt((worldPosition - _leftBottom).z / _cellSize);
+        }
+
+        public Vector3 GetCellOrigin(int x, int z)
+        {
+            return GetWorldPosition(x, z) + new Vector3(_halfCellSize, 0, _halfCellSize);
+        }
+
+        public Vector3 GetCellOrigin(Vector3 worldPosition)
+        {
+            GetGridPosition(worldPosition, out var x, out var z);
+            return GetCellOrigin(x, z);
+        }
+    }
+}
---
+++ Assets/Src/GridSystem/GridDemo.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+
+namespace Src.GridSystem
+{
+    public class GridDemo : MonoBehaviour
+    {
+        private Grid<bool> _grid;
+        private MeshRenderer _renderer;
+        private Vector3 _planeSize;
+
+        private void Start()
+        {
+            _renderer = gameObject.GetComponent<MeshRenderer>();
+            _planeSize = _renderer.bounds.size;
+            var cellSize = 0.4f;
+            var gridWidth = Mathf.CeilToInt(_planeSize.x / cellSize);
+            var gridHeight = Mathf.CeilToInt(_planeSize.z / cellSize);
+            _grid = new Grid<bool>(gridWidth, gridHeight, cellSize, Vector3.zero);
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (_grid == null) return;
+
+            _grid.GetSize(out var width, out var depth);
+            Gizmos.color = Color.white;
+            for (var x = 0; x < width; x++)
+            for (var z = 0; z < depth; z++)
+            {
+                Gizmos.DrawLine(_grid.GetWorldPosition(x, z), _grid.GetWorldPosition(x, z + 1));
+                Gizmos.DrawLine(_grid.GetWorldPosition(x, z), _grid.GetWorldPosition(x + 1, z));
+            }
+
+            Gizmos.DrawLine(_grid.GetWorldPosition(0, depth), _grid.GetWorldPosition(width, depth));
+            Gizmos.DrawLine(_grid.GetWorldPosition(width, 0), _grid.GetWorldPosition(width, depth));
+
+
+            var cameraTransform = Camera.main.transform;
+            if (!Physics.Raycast(cameraTransform.position, cameraTransform.forward, out var hit, 100.0f))
+                return;
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(hit.point, 0.04f);
+            var cellCenter = _grid.GetCellOrigin(hit.point);
+            Gizmos.color = new Color(1f, 1f, 0, 0.5f);
+            Gizmos.DrawCube(cellCenter, new Vector3(_grid.GetCellSize(), 0.1f, _grid.GetCellSize()));
+        }
+    }
+}
