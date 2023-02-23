d8de3a83126f8e948f9b0c3361c97ec0beb0d4e7
Sunbvert <sunchuanop@outlook.com>
2023-02-23 23:32:46 +0100

    Grid System V2

--- Assets/Src/GridDemo.cs
+++ Assets/Src/GridDemo.cs
@@ -15,7 +15,7 @@ namespace Src
 
         [Space] [SerializeField] private GameObject turretTower;
 
-        private GridMap _gridMap;
+        private GridManager _gridManager;
         private bool _editMode;
         private GameObject _editModeTower;
 
@@ -26,73 +26,8 @@ namespace Src
                 gridDepth * gridCellSize / planeSize.z);
             gameObject.transform.localScale = planeScale;
 
-            _gridMap = new GridMap(gridWidth, gridDepth, gridCellSize, Vector3.zero);
+            _gridManager = gameObject.AddComponent<GridManager>();
             _editModeTower = Instantiate(turretTower);
-        }
-
-        public void Update()
-        {
-            if (Input.GetKeyUp(KeyCode.Space)) _editMode = !_editMode;
-
-            if (_editMode)
-            {
-                var cameraTransform = Camera.main.transform;
-                if (!Physics.Raycast(cameraTransform.position, cameraTransform.forward, out var hit, 100.0f))
-                    return;
-
-                _gridMap.GetGridPosition(hit.point, out var x, out var z);
-
-                var currentGridCellCenter = _gridMap.GetCellOrigin(x, z);
-                _editModeTower.transform.position = currentGridCellCenter;
-
-                if (Input.GetKeyUp(KeyCode.P))
-                {
-                    var obj = _gridMap.GetValue(_editModeTower.tag, x, z);
-                    if (!obj)
-                    {
-                        var newTower = Instantiate(turretTower, currentGridCellCenter, new Quaternion());
-                        Debug.Log("放置新的对象，tag为：" + newTower.tag);
-                        _gridMap.SetValue(x, z, newTower);
-                    }
-                }
-
-                if (Input.GetKeyUp(KeyCode.Backspace))
-                {
-                    var obj = _gridMap.GetValue(_editModeTower.tag, x, z);
-                    if (obj)
-                    {
-                        Destroy(obj);
-                        Debug.Log("删除了对象，tag为：" + obj.tag);
-                    }
-                }
-            }
-        }
-
-        private void OnDrawGizmos()
-        {
-            if (_gridMap == null) return;
-
-            _gridMap.GetGridSize(out var width, out var depth);
-            Gizmos.color = Color.white;
-            for (var x = 0; x < width; x++)
-            for (var z = 0; z < depth; z++)
-            {
-                Gizmos.DrawLine(_gridMap.GetCellLeftBottom(x, z), _gridMap.GetCellLeftBottom(x, z + 1));
-                Gizmos.DrawLine(_gridMap.GetCellLeftBottom(x, z), _gridMap.GetCellLeftBottom(x + 1, z));
-            }
-
-            Gizmos.DrawLine(_gridMap.GetCellLeftBottom(0, depth), _gridMap.GetCellLeftBottom(width, depth));
-            Gizmos.DrawLine(_gridMap.GetCellLeftBottom(width, 0), _gridMap.GetCellLeftBottom(width, depth));
-
-
-            var cameraTransform = Camera.main.transform;
-            if (!Physics.Raycast(cameraTransform.position, cameraTransform.forward, out var hit, 100.0f))
-                return;
-            Gizmos.color = Color.blue;
-            Gizmos.DrawSphere(hit.point, 0.04f);
-            var cellCenter = _gridMap.GetCellOrigin(hit.point);
-            Gizmos.color = new Color(1f, 1f, 0, 0.5f);
-            Gizmos.DrawCube(cellCenter, new Vector3(_gridMap.GetCellSize(), 0.1f, _gridMap.GetCellSize()));
         }
     }
 }
--- Assets/Src/GridSystem/Grid.cs
+++ Assets/Src/GridSystem/Grid.cs
@@ -2,155 +2,57 @@
 
 namespace Src.GridSystem
 {
-    public class Grid<TGridItem>
+    public class Grid<T>
     {
         private readonly int _width;
         private readonly int _depth;
         private readonly float _cellSize;
-        private readonly float _halfCellSize;
-        private readonly Vector3 _leftBottom;
-
-        private readonly TGridItem[,] _gridArray;
-
-
-        /// <summary>
-        /// 创建一个网格
-        /// </summary>
-        /// <param name="width">网格的宽度，x轴上的格子数量</param>
-        /// <param name="depth">网格的深度，z轴上的格子数量</param>
-        /// <param name="cellSize">网格的大小</param>
-        /// <param name="originPosition">网格中心的位置</param>
-        public Grid(int width, int depth, float cellSize, Vector3 originPosition)
+        private readonly Vector3 _gridLeftBottom;
+        private readonly T[,] _gridArray;
+        
+        public Grid(int width, int depth, float cellSize, Vector3 gridLeftBottom)
         {
-            var leftBottom = new Vector3(originPosition.x - width * cellSize / 2, 0,
-                originPosition.z - depth * cellSize / 2);
             _width = width;
             _depth = depth;
             _cellSize = cellSize;
-            _halfCellSize = _cellSize / 2;
-            _leftBottom = leftBottom;
-
-            _gridArray = new TGridItem[width, depth];
+            _gridLeftBottom = gridLeftBottom;
+            _gridArray = new T[width, depth];
         }
-
-        /// <summary>
-        /// out返回x和z轴上格子的数量
-        /// </summary>
-        /// <param name="width">x轴上格子的数量</param>
-        /// <param name="depth">z轴上格子的数量</param>
-        public void GetGridSize(out int width, out int depth)
-        {
-            width = _width;
-            depth = _depth;
-        }
-
-        /// <summary>
-        /// 返回世界尺寸正方形格子的宽度
-        /// </summary>
-        /// <returns>世界尺寸格子的宽度</returns>
-        public float GetCellSize()
-        {
-            return _cellSize;
-        }
-
-        /// <summary>
-        /// 获取网格数组
-        /// </summary>
-        /// <returns>网格数组</returns>
-        public TGridItem[,] GetGridArray()
+        
+        public T[,] GetGridArray()
         {
             return _gridArray;
         }
-
-        /// <summary>
-        /// 根据格子数组的位置设置格子的值
-        /// </summary>
-        /// <param name="x">x轴上的第几个格子</param>
-        /// <param name="z">z轴上的第几个格子</param>
-        /// <param name="value">在网格数组此处设置的值</param>
-        public void SetValue(int x, int z, TGridItem value)
+        
+        public void SetValue(int x, int z, T value)
         {
             if (x >= 0 && z >= 0 && x < _width && z < _depth) _gridArray[x, z] = value;
         }
-
-        /// <summary>
-        /// 根据世界坐标的位置设置格子的值
-        /// </summary>
-        /// <param name="worldPosition">世界坐标</param>
-        /// <param name="value">值</param>
-        public void SetValue(Vector3 worldPosition, TGridItem value)
+        
+        public void SetValue(Vector3 worldPosition, T value)
         {
             GetGridPosition(worldPosition, out var x, out var z);
             SetValue(x, z, value);
         }
-
-        /// <summary>
-        /// 根据格子数组的位置获取格子的值
-        /// </summary>
-        /// <param name="x">x轴上的第几个格子</param>
-        /// <param name="z">z轴上的第几个格子</param>
-        /// <returns></returns>
-        public TGridItem GetValue(int x, int z)
+        
+        public T GetValue(int x, int z)
         {
             if (x >= 0 && z >= 0 && x < _width && z < _depth)
                 return _gridArray[x, z];
             else
                 return default;
         }
-
-        /// <summary>
-        /// 根据世界坐标获取格子的值
-        /// </summary>
-        /// <param name="worldPosition">世界坐标</param>
-        /// <returns></returns>
-        public TGridItem GetValue(Vector3 worldPosition)
+        
+        public T GetValue(Vector3 worldPosition)
         {
             GetGridPosition(worldPosition, out var x, out var z);
             return GetValue(x, z);
         }
-
-        /// <summary>
-        /// 获取格子数组位置的世界坐标
-        /// </summary>
-        /// <param name="x">x轴上的第几个格子</param>
-        /// <param name="z">z轴上的第几个格子</param>
-        public Vector3 GetWorldPosition(int x, int z)
-        {
-            return new Vector3(x, 0, z) * _cellSize + _leftBottom;
-        }
-
-        /// <summary>
-        /// 获取世界坐标的格子位置
-        /// </summary>
-        /// <param name="worldPosition">世界坐标</param>
-        /// <param name="x">返回的格子在x轴上的位置</param>
-        /// <param name="z">返回的格子在z轴上的位置</param>
+        
         public void GetGridPosition(Vector3 worldPosition, out int x, out int z)
         {
-            x = Mathf.FloorToInt((worldPosition - _leftBottom).x / _cellSize);
-            z = Mathf.FloorToInt((worldPosition - _leftBottom).z / _cellSize);
-        }
-
-        /// <summary>
-        /// 获取格子中心的世界坐标
-        /// </summary>
-        /// <param name="x">x轴上的第几个格子</param>
-        /// <param name="z">z轴上的第几个格子</param>
-        /// <returns></returns>
-        public Vector3 GetCellOrigin(int x, int z)
-        {
-            return GetWorldPosition(x, z) + new Vector3(_halfCellSize, 0, _halfCellSize);
-        }
-
-        /// <summary>
-        /// 获取格子中心的世界坐标
-        /// </summary>
-        /// <param name="worldPosition">世界坐标</param>
-        /// <returns></returns>
-        public Vector3 GetCellOrigin(Vector3 worldPosition)
-        {
-            GetGridPosition(worldPosition, out var x, out var z);
-            return GetCellOrigin(x, z);
+            x = Mathf.FloorToInt((worldPosition - _gridLeftBottom).x / _cellSize);
+            z = Mathf.FloorToInt((worldPosition - _gridLeftBottom).z / _cellSize);
         }
     }
 }
